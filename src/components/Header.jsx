5f6193209b145a8b7e76d82f035c8e7197921355
Tara <olasojinifemi67@gmail.com>
2025-12-08 19:29:05 +0100

    feat: add logo image to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'; 
 import { Play, Save, Share2, Settings, Moon, Sun, Plus, X, Menu, Code } from 'lucide-react';
+import logo from '../assets/logo.png';
 
 export default function Header ({ 
         isDark, 
@@ -19,7 +20,7 @@ export default function Header ({
         <header className={` dark:bg-gray-900 border-b border-gray-700 px-4 py-3 flex items-center justify-between 
         flex-wrap gap-3`}> 
             <div className="flex items-center gap-4 flex-wrap"> 
-                <h1 className="text-blue-500 font-bold text-lg flex items-center gap-2"> <img src="src\assets\logo.png" alt="logo-image" className='w-[23px]' /> CodeEditor</h1> 
+                <h1 className="text-blue-500 font-bold text-lg flex items-center gap-2"> <img src={logo} alt="logo-image" className='w-[23px]' /> CodeEditor</h1> 
                 <button className={`cursor-pointer px-4 py-1.5 rounded flex items-center gap-2 text-sm font-medium transition ${
                     isRunning
                         ? 'bg-gray-600 cursor-not-allowed'
